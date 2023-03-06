806cdad7a324e7f8ade9f894c4eeaf9b75042fe1
vishnu prakash <vishnuvictorio@gmail.com>
2023-03-06 19:21:00 +0530

    Introduced Video container.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { React } from 'react';
 import './App.scss';
 import Index from './components/SideBar';
+import VideoDetails from './components/VideoDetails';
 
 const App = () =>
 	<div className="App">
 		<Index/>
+		<VideoDetails/>
 	</div>;
 
 export default App;
---
+++ src/components/VideoDetails.js
@@ -0,0 +1,17 @@
+import { React } from 'react';
+
+const VideoDetails = () =>
+	<div className="container">
+		<div className="block-1">
+			<div className="box-1">A</div>
+			<div className="box-2">B</div>
+			<div className="box-3">C</div>
+		</div>
+		<div className="block-2">
+			<div className="box-4">D</div>
+			<div className="box-5">E</div>
+			<div className="box-6">F</div>
+		</div>
+	</div>;
+
+export default VideoDetails;
