8891ddfcea72aca0a3bf7d9fc1fceff5630b24f7
megalamaggi <megala03.2002@gmail.com>
2023-04-04 19:13:37 +0530

    Refactored NavBar.

--- src/components/SideBar/ExtendedSideBar/BrowseChannels.js
+++ src/components/SideBar/ExtendedSideBar/BrowseChannels.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import NavBar from '../../NavBar/NavBar';
 import SideBar from '../SideBar';
 import ChannelCategories from './BrowseChannels/ChannelCategories';
 
 const BrowseChannels = (context) =>
-	<div>
-		<NavBar { ...context }/>
-		<div className="container">
-			<SideBar { ...context }/>
-			<ChannelCategories { ...context }/>
-		</div>
+	<div className="container">
+		<SideBar { ...context }/>
+		<ChannelCategories { ...context }/>
 	</div>;
 
 export default BrowseChannels;
--- src/components/SideBar/ExtendedSideBar/Music/Music.js
+++ src/components/SideBar/ExtendedSideBar/Music/Music.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import NavBar from '../../../NavBar/NavBar';
 import SideBar from '../../SideBar';
 import MusicCarousel from './MusicCarousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Music = (context) =>
-	<div>
-		<NavBar { ...context }/>
-		<div className="container">
-			<SideBar { ...context }/>
-			<MusicCarousel/>
-		</div>
+	<div className="container">
+		<SideBar { ...context }/>
+		<MusicCarousel/>
 	</div>;
 
 export default Music;
