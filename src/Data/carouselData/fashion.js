ebc55c000a0618ca7dd0e0f6eb8f036a39acb920
jeevan kumar <jeevankumarhari2002@gmail.com>
2023-04-11 17:11:54 +0530

    Introduced CarouselData.

---
+++ src/Data/carouselData/fashion.js
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+
+const fashion = [
+	{
+		category: 'Chanel',
+		video: 'https://player.vimeo.com/external/396336975.sd.mp4?s=97193b0befb8a70e7426ac18d3c449d3fbd06639&profile_id=164&oauth2_token_id=57447761',
+		subscribers: '249K subscribers',
+	},
+	{
+		category: 'Louis vuitton',
+		video: 'https://player.vimeo.com/external/391961403.sd.mp4?s=02912c82997b58c4a4f5aef5cf1f206a35442171&profile_id=164&oauth2_token_id=57447761',
+		subscribers: '485K subscribers',
+	},
+	{
+		category: 'Balenciaga',
+		video: 'https://player.vimeo.com/external/397056851.sd.mp4?s=ebdb6403c0be2432479dcf7068624a51fb2163c6&profile_id=164&oauth2_token_id=57447761',
+		subscribers: '14M subscribers',
+	},
+];
+
+export default fashion;
---
+++ src/Data/carouselData/music.js
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+
+const music = [
+	{
+		category: 'Yolanda Renee',
+		video: 'https://player.vimeo.com/external/335640655.sd.mp4?s=de7c801e6f0833eb7dbbc73602f2aaf2a040f2b7&profile_id=164&oauth2_token_id=57447761',
+		subscribers: '249K subscribers',
+	},
+	{
+		category: 'Sally jo',
+		video: 'https://player.vimeo.com/external/306390278.sd.mp4?s=9517caec783132f6f0fb3078b495a398ec4e886b&profile_id=164&oauth2_token_id=57447761',
+		subscribers: '485K subscribers',
+	},
+	{
+		category: 'Rclbeauty',
+		video: 'https://player.vimeo.com/external/371855028.sd.mp4?s=91381db6457e4abe849285127a4188fb5cc481db&profile_id=164&oauth2_token_id=57447761',
+		subscribers: '14M subscribers',
+	},
+];
+
+export default music;
