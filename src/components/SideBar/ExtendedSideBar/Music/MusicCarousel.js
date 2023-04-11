8a9955ef572acd95823460b07f8153491414d497
vishnu prakash <vishnuvictorio@gmail.com>
2023-04-11 17:21:54 +0530

    Introuced  text over video.

--- src/components/SideBar/ExtendedSideBar/Music/MusicCarousel.js
+++ src/components/SideBar/ExtendedSideBar/Music/MusicCarousel.js
@@ -7,29 +7,41 @@ const MusicCarousel = () =>
 	<div className="music_container">
 		<Carousel
 			autoPlay={ true }
-			showThumbs={ false }
 			className="carousel"
 		>
-
 			<div>
-				<img
-					alt=""
-					src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png"
-				/>
+				<video autoPlay={ true } className="video_carousel">
+					<source
+						src="https://player.vimeo.com/external/324296539.sd.mp4?s=806775db4de3140951b439ef4b2a69c8bd91fa0f&profile_id=164&oauth2_token_id=57447761"
+						type="video/mp4"
+					/>
+				</video>
+				<div className="inner">
+					<h1 style={ { color: 'white' } }> Team Dinner at Seashells</h1>
+				</div>
 
 			</div>
 			<div>
-				<img
-					alt=""
-					src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png"
-				/>
+				<video autoPlay={ true } className="video_carousel">
+					<source
+						src="https://player.vimeo.com/external/174002832.sd.mp4?s=0ca35de91c0ee05b84b55543c19cd0e47aa67a80&profile_id=164&oauth2_token_id=57447761"
+						type="video/mp4"
+					/>
+				</video>
+				<div className="inner">
+					<h1 style={ { color: 'white' } }> Must for Guitar Starters</h1>
+				</div>
 			</div>
 			<div>
-				<img
-					alt=""
-					src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png"
-				/>
-
+				<video autoPlay={ true } className="video_carousel">
+					<source
+						src="https://player.vimeo.com/external/322424304.sd.mp4?s=8acd9f03f32aaca13e6014bcf7ac77135557630f&profile_id=164&oauth2_token_id=57447761"
+						type="video/mp4"
+					/>
+				</video>
+				<div className="inner">
+					<h1 style={ { color: 'white' } }> Must for Guitar Starters</h1>
+				</div>
 			</div>
 		</Carousel>
 	</div>
