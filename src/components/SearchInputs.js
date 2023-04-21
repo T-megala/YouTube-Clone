398aa19b17f44b9a47b10515446402ef30af09d3
megalamaggi <megala03.2002@gmail.com>
2023-04-21 12:43:41 +0530

    Refactored SearchInput.

--- src/components/SearchInputs.js
+++ src/components/SearchInputs.js
@@ -17,24 +17,26 @@ const SearchInputs = (context) => {
 
 	return <div className="container">
 		<SideBar { ...context }/>
-		<div className="videoHistoryContainer">
-			{filteredVideo.map((detail, key) =>
-				<Link
-					key={ key }
-					onClick={ () => setState({
-						...state,
-						selectedId: `${ detail.video }`,
-						history: [...history, detail],
-					}) }
-					to={ `/video${ detail.id }` }
-					style={ { textDecoration: 'none', color: 'black' } }
-					className="box-1"
-				>
-					<Thumbnail { ...{ ...context, data: detail } }/>
-					<div className="channelOnly">
-						<Channel { ...{ ...context, data: detail } }/>
-					</div>
-				</Link>)}
+		<div className="hist">
+			<div className="videoHistoryContainer">
+				{filteredVideo.map((detail, key) =>
+					<Link
+						key={ key }
+						onClick={ () => setState({
+							...state,
+							selectedId: `${ detail.video }`,
+							history: [...history, detail],
+						}) }
+						to={ `/video${ detail.id }` }
+						style={ { textDecoration: 'none', color: 'black' } }
+						className="box-1"
+					>
+						<Thumbnail { ...{ ...context, data: detail } }/>
+						<div className="channelOnly">
+							<Channel { ...{ ...context, data: detail } }/>
+						</div>
+					</Link>)}
+			</div>
 		</div>
 	</div>;
 };
